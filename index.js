d1c833887a97b9765eebd558656ffaefe4b9c10d
Rick Mak <rick.mak@gmail.com>
2016-11-07 11:54:58 +0800

    Init commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const skygearCloud = require('skygear/cloud');
+const handler = require('./src/handler');
+
+skygearCloud.op('op1', function(opts) {
+  console.log('op1');
+  console.log(opts);
+  return {
+    'status': 'ok'
+  };
+});
+
+skygearCloud.op('op:user', function() {
+  console.log('hander1');
+}, {
+  "userRequired": true,
+  "keyRequired": true
+});
+
+skygearCloud.every('@every 60s', function minute() {
+  console.log('every 60');
+  return {
+    'time': '1 minute'
+  }
+});
+
+skygearCloud.every('@every 30s', function() {
+  console.log('every 30');
+});
+
+skygearCloud.beforeSave('note', function user1() {
+  console.log('beforeSave note');
+}, {
+  "async": false
+});
+skygearCloud.afterSave('note', function user1() {
+  console.log('afterSave note');
+});
+skygearCloud.beforeDelete('note', function() {
+  console.log('beforeDelete note');
+});
+skygearCloud.afterDelete('note', function() {
+  console.log('afterDelete note');
+});
---
+++ src/handler.js
@@ -0,0 +1,5 @@
+const skygearCloud = require('skygear/cloud');
+
+skygearCloud.handler('handler1', function() {
+  console.log('hander1');
+});
